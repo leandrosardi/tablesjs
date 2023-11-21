864ca50ddf275e5933e422583a0452038c5158dd
leandrosardi <leandro.sardi@expandedventure.com>
2023-11-21 09:38:38 -0300

    #1

---
+++ app.rb
@@ -0,0 +1,25 @@
+get '/tablesjs', :agent => /(.*)/ do
+    erb :"/extensions/filtersjs/views/example", :layout => :'/views/layouts/public'
+end
+
+get '/tablesjs/', :agent => /(.*)/ do
+    erb :"/extensions/filtersjs/views/example", :layout => :'/views/layouts/public'
+end
+
+=begin
+get '/filtersjs', :agent => /(.*)/ do
+    redirect '/developers/filtersjs'
+end
+
+get '/filtersjs/', :agent => /(.*)/ do
+    redirect '/developers/filtersjs'
+end
+
+get '/developers/filtersjs', :agent => /(.*)/ do
+    erb :"/extensions/filtersjs/views/example", :layout => :'/views/layouts/public'
+end
+
+get '/developers/filtersjs/', :agent => /(.*)/ do
+    erb :"/extensions/filtersjs/views/example", :layout => :'/views/layouts/public'
+end
+=end
